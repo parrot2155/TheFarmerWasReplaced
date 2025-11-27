6878b99225e64637d6bbc20dbb81b9cdd2ea238e
Park Tae Wook <parrot2155@gmail.com>
2025-11-27 19:54:25 +0900

    [day2]
    당근 생산 자동화

---
+++ harvest_carrot.py
@@ -0,0 +1,13 @@
+def harv_carrot():
+#당근 수확 함수
+    while num_items(Items.Hay)>=1000 and num_items(Items.Wood)>=500:
+        for i in range(get_world_size()):
+            for i in range(get_world_size()):
+                for j in range(get_world_size()):
+                    if can_harvest():
+                        harvest()
+                    if get_ground_type() != Grounds.Soil:
+                        till()
+                    plant(Entities.Carrot)
+                    move(North)    
+                move(East)
--- harvest_log.py
+++ harvest_log.py
@@ -1,23 +1,29 @@
+# def harv_wood():
+# #나무 수확 함수
+#     while num_items(Items.Wood)<=500:
+#         for i in range(get_world_size()):
+#             for j in range(get_world_size()):
+#                 if can_harvest():
+#                     harvest()
+#                 if get_ground_type() != Grounds.Grassland:
+#                     till()
+#                 plant(Entities.Bush)             
+#                 move(North)
+#             move(East)
+    
 def harv_wood():
 #나무 수확 함수
-    while num_items(Items.Wood)<=500:
-        for i in range(get_world_size()):
-            for j in range(get_world_size()):
+    clear()
+    while num_items(Items.Wood)<=3000:
+        for i in range(get_world_size()/2):
+            for j in range(get_world_size()/2):
                 if can_harvest():
                     harvest()
-                if get_ground_type() != Grounds.Grassland:
-                    till()
-                plant(Entities.Bush)             
+                    if get_ground_type() != Grounds.Grassland:
+                        till()
+                    plant(Entities.Tree)
+                    move(North)
+                    move(North)
+                move(East)
                 move(North)
-            move(East)
-    
-
-# for i in range(get_world_size()):
-#     for j in range(get_world_size()):
-#         if can_harvest():
-#             harvest()
-#         if get_ground_type() != Grounds.Grassland:
-#             till()
-#         plant(Entities.Bush)
-#         move(North)
         
