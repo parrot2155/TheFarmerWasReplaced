32b4290ef05dfc7fd5fff32e09d4caee07aaa34b
Park Tae Wook <parrot2155@gmail.com>
2025-12-01 23:47:41 +0900

    [day05] sunflower 최댓값만 뽑기
    해바라기 최댓값만 수확하는 기능 추가

--- harvest_sunflower.py
+++ harvest_sunflower.py
@@ -1,14 +1,42 @@
 def harv_sunf():
 #해바라기 수확 함수
-    while num_items(Items.Power)<=100000:
-        for i in range(get_world_size()):
-            for j in range(get_world_size()):
-                if can_harvest():
-                    harvest()
-                if get_ground_type() != Grounds.Soil:
-                    till()
-                plant(Entities.Sunflower)
-                # if get_water()<=0.75:
-                #     use_item(Items.Water)
-                move(North)    
-            move(East)
+    # while num_items(Items.Power)<=10000:
+    #     for i in range(get_world_size()):
+    #         for j in range(get_world_size()):
+    #             if can_harvest():
+    #                 harvest()
+    #             if get_ground_type() != Grounds.Soil:
+    #                 till()
+    #             plant(Entities.Sunflower)
+    #             # if get_water()<=0.75:
+    #             #     use_item(Items.Water)
+    #             move(North)    
+    #         move(East)
+    k = 1
+    while True:
+        if k%2==1:
+            for i in range(get_world_size()):
+                for j in range(get_world_size()):
+                    if can_harvest():
+                        harvest()
+                    if get_ground_type() != Grounds.Soil:
+                        till()
+                    plant(Entities.Sunflower)
+                    # if get_water()<=0.75:
+                    #     use_item(Items.Water)
+                    move(North)    
+                move(East)
+        else:
+            max=15
+            while max>=7:
+                for i in range(get_world_size()):
+                    for j in range(get_world_size()):
+                        if measure()==max:
+                            if can_harvest():
+                                harvest()
+                        move(North)    
+                    move(East)
+                max=max-1
+        k=k+1
+        
+        
