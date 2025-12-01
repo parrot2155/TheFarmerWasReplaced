5ca944210c5439c6f341b44951ba4314deca0d29
Park Tae Wook <parrot2155@gmail.com>
2025-12-01 23:10:21 +0900

    [day05]

--- harvest_log.py
+++ harvest_log.py
@@ -13,30 +13,29 @@
     
 def harv_wood():
 #나무 수확 함수
-    # clear()
-    # while num_items(Items.Wood)<=2000000:
-    #     for i in range(get_world_size()/2):
-    #         for j in range(get_world_size()/2):
-    #             if can_harvest():
-    #                 harvest()
-    #                 if get_ground_type() != Grounds.Grassland:
-    #                     till()
-    #                 plant(Entities.Tree)
-    #                 # if get_water()<=0.75:
-    #                 #     use_item(Items.Water)
-    #             move(North)
-    #             move(North)
-    #         move(East)
-    #         move(North)
-
-    while num_items(Items.Wood)<=2000000:
-        for i in range(get_world_size()):
-            for j in range(get_world_size()):
+    while num_items(Items.Wood)<=200000:
+        for i in range(get_world_size()/2):
+            for j in range(get_world_size()/2):
                 if can_harvest():
                     harvest()
-                if get_ground_type() != Grounds.Grassland:
-                    till()
-                plant(Entities.Tree)             
+                    if get_ground_type() != Grounds.Grassland:
+                        till()
+                    plant(Entities.Tree)
+                    # if get_water()<=0.75:
+                    #     use_item(Items.Water)
+                move(North)
                 move(North)
             move(East)
+            move(North)
+
+    # while num_items(Items.Wood)<=200000:
+    #     for i in range(get_world_size()):
+    #         for j in range(get_world_size()):
+    #             if can_harvest():
+    #                 harvest()
+    #             if get_ground_type() != Grounds.Grassland:
+    #                 till()
+    #             plant(Entities.Tree)             
+    #             move(North)
+    #         move(East)
         
--- harvest_sunflower.py
+++ harvest_sunflower.py
@@ -1,6 +1,6 @@
 def harv_sunf():
 #해바라기 수확 함수
-    while num_items(Items.Power)<=100000000:
+    while num_items(Items.Power)<=100000:
         for i in range(get_world_size()):
             for j in range(get_world_size()):
                 if can_harvest():
